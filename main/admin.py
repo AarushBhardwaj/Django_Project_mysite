d4fa62329219314df6a791420570a2a1709189a4
AarushBhardwaj <aarushbhardwaj78@gmail.com>
2023-02-28 19:50:10 -0500

    First Commit

---
+++ main/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import Tutorial, TutorialCategory, TutorialSeries
+from tinymce.widgets import TinyMCE
+from django.db import models
+
+# Register your models here.
+
+
+class TutorialAdmin(admin.ModelAdmin):
+	# fields = ["tutorial_title",
+	# 		  "tutorial_published",
+	# 		  "tutorial_content"]
+
+	# To cluster Fields together
+
+	fieldsets = [
+			("Title/Date", {"fields": ["tutorial_title", "tutorial_published"]}),
+			("URL", {"fields": ["tutorial_slug"]}),
+			("Series", {"fields": ["tutorial_series"]}),
+			("Content", {"fields": ["tutorial_content"]})
+
+	]
+
+	formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+        }
+
+admin.site.register(TutorialCategory)
+admin.site.register(TutorialSeries)
+admin.site.register(Tutorial, TutorialAdmin)
+
